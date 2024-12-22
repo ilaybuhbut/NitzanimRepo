4de19af44f32029696b495292d05429fe897037a
ilaybuhbut <ilayforshcool@gmail.com>
2024-12-22 19:26:22 +0200

    new commit

--- Ilay.py
+++ Ilay.py
@@ -1,2 +1,5 @@
 def print_funny_story():
     print("one time me and roei finished school and started going home. when we got out of school we saw a car driving towords us and it played really loud itay levy music. as the car pulled up to the school we saw the canadian girl screaming in a very canadian accent nooo mommm you are emberessing meeee")
+
+def print_childhood_story():
+    print(" אני שיחקתי אייפקס יום אחד והייתי ברצף נצחונות של שלוש ואז אוראם קרא לי לשחק עם ניראל והוא מכר את המשחק והפסדתי את הרצף עד היום אני כל הזמן מזכיר את זה כשמישהו מדבר על ניראל ופשוט נזכר בזה כל הזמן ועברו איזה 5 שנים")
