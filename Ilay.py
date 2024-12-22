5d3201bc9be721e50b4d09cd6f0f58ddb135ba60
ilaybuhbut <ilayforshcool@gmail.com>
2024-12-22 18:56:27 +0200

    funny story haha

---
+++ Ilay.py
@@ -0,0 +1,2 @@
+def print_funny_story():
+    print("one time me and roei finished school and started going home. when we got out of school we saw a car driving towords us and it played really loud itay levy music. as the car pulled up to the school we saw the canadian girl screaming in a very canadian accent nooo mommm you are emberessing meeee")
